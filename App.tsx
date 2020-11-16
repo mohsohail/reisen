7fcab14705c73e0c39328e75706dc4c97cad5553
mohsohail <sohail.momin1@gmail.com>
2020-11-16 12:11:26 +0530

    intermediate (fullscreen images view)

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 
 export default function App() {
+  const src = "./assets/feed/images/josh-hild-Y4SGF6YBmyw-unsplash.jpg";
+  // const [ratio, setRatio] = useState(1);
+  // useEffect(() => {
+  //   if (src) {
+  //     Image.getSize(src, (width, height) => {
+  //       setRatio(width / height);
+  //     });
+  //   }
+  // }, [src]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <ScrollView contentContainerStyle={{ alignItems: "center" }}> */}
+      {/* <Text>Hello World</Text> */}
+      <View>
+        <Image
+          style={styles.feedImage}
+          // style={{ width: "100%", height: "100%", aspectRatio: ratio }}
+          source={require(src)}
+          // resizeMode="contain"
+        />
+      </View>
+      {/* <StatusBar style="auto" /> */}
+      {/* </ScrollView> */}
     </View>
   );
 }
@@ -14,8 +33,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  feedImage: {
+    width: "100%",
+    height: "100%",
   },
 });
